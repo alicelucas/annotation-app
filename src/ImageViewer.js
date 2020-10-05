c5d3c22cf3b2ea9ac9da5eeea535c1864487b9f7
alucas <alicelucas93@gmail.com>
2020-10-05 11:14:17 -0400

    using Drei's PerspectiveCamera

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import {Canvas} from 'react-three-fiber'
-import {OrbitControls} from "@react-three/drei";
+import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
 import {ImageCanvas} from "./ImageCanvas";
 
 export const ImageViewer = () => {
     return (
         <Canvas
-            camera={{fov: 80, near:0.1, far:5000, position: [0, 0, 1000] }}
+            //camera={{fov: 80, near:0.1, far:5000, position: [0, 0, 1000] }}
             colorManagement={false}
             onCreated={ ({gl}) => {
                 gl.setClearColor('black')
             }}
         >
+            <PerspectiveCamera makeDefault position={[0, 0, 1]}/>
             <ImageCanvas/>
             <OrbitControls enableRotate={false} />
         </Canvas>
