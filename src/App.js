752944a9a6cdb3e92bb2b6895d13cc8bfb4c077d
alucas <alicelucas93@gmail.com>
2020-10-09 15:32:18 -0400

    shaders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import './App.css';
 import {ImageViewer} from "./ImageViewer.js"
 import {Canvas} from "react-three-fiber";
 import {OrbitControls, PerspectiveCamera, TransformControls} from "@react-three/drei";
 import {ImageCanvas} from "./ImageCanvas";
 import {ImageMesh} from "./ImageMesh";
+import {Controls, ControlsProvider, useControl} from "react-three-gui";
+import {BrightnessSlider} from "./BrightnessSlider";
 
 function App() {
-  return (
+    const [brightness, setBrightness] = useState(0.0)
+
+    const onBrightnessChange = (value) => {
+        setBrightness(value)
+    }
+
+    return (
     <div className="App">
+        <BrightnessSlider onChange={onBrightnessChange} value={brightness}/>
         <Canvas
             //camera={{fov: 80, near:0.1, far:5000, position: [0, 0, 1000] }}
             colorManagement={false}
@@ -17,11 +26,12 @@ function App() {
             }}
         >
             <PerspectiveCamera makeDefault position={[0, 0, 2]}/>
-            <TransformControls mode={"translate"}>
+            //TODO Go back to using TransformControls when you have figured out proper panning.
+            {/*<TransformControls mode={"translate"}>*/}
                 <Suspense fallback={null}>
-                    <ImageMesh/>
+                    <ImageMesh brightness={brightness}/>
                 </Suspense>
-            </TransformControls>
+            {/*</TransformControls>*/}
             <OrbitControls enableRotate={false} />
         </Canvas>
     </div>
--- src/ImageMesh.js
+++ src/ImageMesh.js
@@ -1,27 +1,56 @@
 import {Box, useTextureLoader} from "@react-three/drei";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import bioImage from "./textures/cho44.png";
+import bioImage2 from "./textures/01_POS002_F.TIF"
 import Image from "image-js";
 
-export const ImageMesh = () => {
+const vertexShader = `
+  varying vec2 vTextureCoord;
+  
+  void main() {
+    vTextureCoord = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  }
+`;
+
+const fragmentShader = `  
+  uniform float brightness;
+  uniform sampler2D uSampler;
+  varying vec2 vTextureCoord;
+  void main() {
+    gl_FragColor = texture2D(uSampler, vTextureCoord);
+    gl_FragColor.rgb += brightness;   
+  }
+`;
+
+export const ImageMesh = (props) => {
     const [image, setImage] = useState(new Image())
-    const [hovered, setHover] = useState(false)
 
     useEffect( () => {
         const fetchImage = async () => {
             const img = await Image.load(bioImage)
             setImage(img)
         }
-        fetchImage().catch( () => {console.log("Error while loading image.")})
+        fetchImage().catch( (reason) => {console.log("Error while loading image: " + reason)})
     },[])
 
-    const gaussian = image.gaussianFilter({radius: 5})
-    const texture = useTextureLoader(gaussian.toDataURL())
+    //TODO: toDataURL may be returning a promise. You have to handle it. I think this is the reason
+    //why the screen is black sometimes.
+    // const texture = useTextureLoader(image.toDataURL())
+    const texture = useTextureLoader(bioImage)
+    const uniforms = {
+        uSampler: {
+            value: texture
+        },
+        brightness: {
+            value: props.brightness
+        },
+    };
 
     return(
-        <mesh onPointerOver={ (e) => setHover(true)} onPointerOut={(e) => setHover(false)}>
+        <mesh>
             <Box args={[image.width / image.height, 1]}>
-                <meshBasicMaterial attach="material" map={texture}/>
+                <shaderMaterial attach="material" vertexShader={vertexShader} fragmentShader={fragmentShader} uniforms={uniforms}/>
             </Box>
         </mesh>
         )
